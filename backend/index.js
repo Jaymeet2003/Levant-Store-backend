28ba1f5505361378a984c688c4164be52f147eb6
Jaymeet2003 <jaymeet5087d@gmail.com>
2024-12-23 21:18:51 -0600

    prod

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // const allowedOrigins = [
 //   'https://admin.yourapp.com', // Admin frontend URL
